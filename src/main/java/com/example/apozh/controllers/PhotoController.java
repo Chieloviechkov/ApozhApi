8e8c8e4b945968bb8787e61c8bcccd2cc17acd09
Chieloviechkov <130806581+Chieloviechkov@users.noreply.github.com>
2023-12-22 21:29:45 +0200

    fix

--- src/main/java/com/example/apozh/controllers/PhotoController.java
+++ src/main/java/com/example/apozh/controllers/PhotoController.java
@@ -4,8 +4,12 @@ import com.example.apozh.Repository.PhotoRepository;
 import com.example.apozh.entity.Photo;
 import com.example.apozh.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,8 +32,13 @@ public class PhotoController {
         return CompletableFuture.supplyAsync(() -> photoService.scrapeAndSavePhotos());
     }
     @GetMapping("/all")
-    public List<Photo> all(){
-        System.out.println(photoRepository.findAll());
-       return photoRepository.findAll();
+    public List<Photo> all(@RequestParam(defaultValue = "0") int page,
+                           @RequestParam(defaultValue = "10") int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Photo> photoPage = photoRepository.findAll(pageable);
+        List<Photo> photos = photoPage.getContent();
+        System.out.println(photos);
+        return photos;
     }
+
 }
